6cccf094cfea188e7a515d023d28ea852889ddfe
Ryan <rhazzardphoto@gmail.com>
2020-02-09 22:37:37 -0800

    schema.sql

--- api/models/schema.sql
+++ api/models/schema.sql
@@ -28,8 +28,23 @@ first_name varchar(64) not null,
 last_name varchar(64) not null
 );
 
+create table saved_listings (
+listing_id int references listings(id), 
+user_id int references users(id)
+); 
+
+create table saved_professional_Listings(
+listing_id int references listings(id), 
+professional_user_id int references professional_users(id)
+);
+
+create table saved_admin_Listings(
+listing_id int references listings(id), 
+admin_user_id int references admin_users(id)
+);
+
 create table listings(
-id serial not null,
+id int primary key not null,
 professional_id int references professional_users(id), 
 business_title varchar(64) not null, 
 business_description varchar(128) not null, 
@@ -64,3 +79,78 @@ answer2 varchar(128),
 lat float, 
 lng float
 ); 
+
+create table pending_listings (
+id serial primary key not null, 
+professional_id int references professional_users(id), 
+business_title varchar(64) not null, 
+business_description varchar(128) not null, 
+street_address varchar(64), 
+city varchar(64), 
+state varchar(32), 
+zip varchar(64),
+phone varchar(24), 
+email varchar(64), 
+website varchar(128), 
+category varchar (64), 
+monday varchar (64), 
+tuesday varchar (64),
+wednesday varchar (64),
+thursday varchar (64),
+friday varchar (64),
+saturday varchar (64),
+sunday varchar (64),
+instagram varchar(128),
+twitter varchar(128),
+linkedin varchar(128),
+facebook varchar(128),
+youtube varchar(128), 
+mission_statement varchar(256), 
+about varchar(256), 
+faq0 varchar(128),
+answer0 varchar(128), 
+faq1 varchar(128),
+answer1 varchar(128), 
+faq2 varchar(128), 
+answer2 varchar(128), 
+lat float, 
+lng float
+);
+
+create table inactive_listings (
+id int primary key not null, 
+professional_id int references professional_users(id), 
+business_title varchar(64) not null, 
+business_description varchar(128) not null, 
+street_address varchar(64), 
+city varchar(64), 
+state varchar(32), 
+zip varchar(64),
+phone varchar(24), 
+email varchar(64), 
+website varchar(128), 
+category varchar (64), 
+monday varchar (64), 
+tuesday varchar (64),
+wednesday varchar (64),
+thursday varchar (64),
+friday varchar (64),
+saturday varchar (64),
+sunday varchar (64),
+instagram varchar(128),
+twitter varchar(128),
+linkedin varchar(128),
+facebook varchar(128),
+youtube varchar(128), 
+mission_statement varchar(256), 
+about varchar(256), 
+faq0 varchar(128),
+answer0 varchar(128), 
+faq1 varchar(128),
+answer1 varchar(128), 
+faq2 varchar(128), 
+answer2 varchar(128), 
+lat float, 
+lng float, 
+reason varchar(24)
+);
