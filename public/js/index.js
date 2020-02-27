00a1d24c37535999013cf20281177edc682a234c
Ryan Hazzard <rhazzardphoto@gmail.com>
2020-02-27 09:07:41 -0800

    styling home page

--- public/js/index.js
+++ public/js/index.js
@@ -188,4 +188,43 @@ $(document).ready(function() {
   $("body").on("click", "#landing-list-a", function() {
     window.location.assign("listing.form.html");
   });
+
+  setTimeout($('.landing-images').slick({
+    // dots: true,
+    autoplay: true, 
+    infinite: false,
+    speed: 1000,
+    arrows: false,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  }), 100);
 });
